722825b9d26af1d4499ac251dfa67fe734c000da
Matus Ivanecky <maty535@users.noreply.github.com>
2018-09-28 13:47:22 +0200

    Update PDF.php
    When EURO symbol is in form '€' => '&#0128;' pdf generation is not working and EURO sign is geenrated as empty rectangle. 
    
    When I changed it to &euro; , everything is generated without problem.

--- src/PDF.php
+++ src/PDF.php
@@ -218,7 +218,7 @@ class PDF{
 
     protected function convertEntities($subject){
         $entities = array(
-            '€' => '&#0128;',
+            '€' => '&euro;',
             '£' => '&pound;',
         );
 
